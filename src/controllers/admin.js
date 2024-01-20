624a9d0d9995ce9b6e401c7ee7a7f9f1dfa3b565
fahiz <fahizk100@gmail.com>
2024-01-20 23:18:22 +0530

    hash password admin

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,6 +1,7 @@
 const admin = require('../model/admin');
-const {setAdmin} = require('../service/auth')
-const {v4: uuidv4} =require  ('uuid')
+const { setAdmin } = require('../service/auth');
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
 const showLoginForm = (req, res) => {
   res.render('admin/login');
@@ -10,18 +11,20 @@ async function handleAdminLogin(req, res) {
   const { email, password } = req.body;
 
   try {
-    const user = await admin.findOne({ email, password });
+    const bcryptpassword = await bcrypt.hash(password, 10);
+    // console.log(bcryptpassword);
 
-    if (!user) {
-      // If the user is not found, render the login form with an error message
+    const user = await admin.findOne({ email });
+    console.log( user);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      // Invalid email or password
       return res.status(401).render('admin/login', { error: 'Invalid email or password' });
     }
-    
-    const sessionId = uuidv4();
-    setAdmin (sessionId,user)
-    res.cookie("uid",sessionId)
-    res.status(200).render('admin/index')
 
+    const sessionId = uuidv4();
+    setAdmin(sessionId, user);
+    res.cookie('uid', sessionId);
+    res.status(200).render('admin/index');
   } catch (error) {
     // Handle any errors that might occur during the database query
     console.error('Error during admin login:', error);
